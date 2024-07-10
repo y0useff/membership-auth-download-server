b756ddcf9b34f162e586449e862cd786050bd8b5
y0useff <therealyousefghaly@gmail.com>
2024-07-10 18:16:23 -0400

    made bug fixes, implemented m3u8 -> mp4 conversion + cdn uploading / redirect to download

--- index.js
+++ index.js
@@ -5,30 +5,22 @@ const port = 3000
 const { test, expect } = require('@playwright/test');
 const playwright = require('playwright');
 const fs = require('fs')
-
 const https = require('https')
 
 
-const REGION = 'New York, US'; // If German region, set this to an empty string: ''
-const BASE_HOSTNAME = 'storage.bunnycdn.com';
-const HOSTNAME = REGION ? `${REGION}.${BASE_HOSTNAME}` : BASE_HOSTNAME;
-const STORAGE_ZONE_NAME = 'delicious-soap-stor';
-const FILENAME_TO_UPLOAD = 'test.txt';
-const FILE_PATH = './test.txt';
-const ACCESS_KEY = '2708e727-6642-427a-b245-1c60c5d07c800bad7415-625f-497b-a070-4acc67ab461e';
-
 const m3u8ToMp4 = require("m3u8-to-mp4");
-const converter = new m3u8ToMp4();
-// (async function() {
-//   await converter
-//     .setInputFile("https://<URL_OF_THE_WEBSITE>/<PATH_TO_THE_M3U8_FILE>")
-//     .setOutputFile("dummy.mp4")
-//     .start();
+const converter = new m3u8ToMp4()
 
-//   console.log("File converted");
-// })();
+const {REGION, BASE_HOSTNAME, STORAGE_ZONE_NAME, ACCESS_KEY, CDN_LINK} = require("./config.json")
 
-const uploadFile = async () => {
+
+const HOSTNAME = REGION ? `${REGION}.${BASE_HOSTNAME}` : BASE_HOSTNAME;
+
+
+const uploadFile = async (titleId) => {
+    console.log("Uploading")
+    const FILE_PATH = `${titleId}.mp4`
+    const FILENAME_TO_UPLOAD = `${titleId}.mp4`
     const readStream = fs.createReadStream(FILE_PATH);
   
     const options = {
@@ -44,6 +36,7 @@ const uploadFile = async () => {
     const req = https.request(options, (res) => {
       res.on('data', (chunk) => {
         console.log(chunk.toString('utf8'));
+        return true;
       });
     });
   
@@ -52,39 +45,43 @@ const uploadFile = async () => {
     });
   
     readStream.pipe(req);
+
+
 };
 
-const main = async () => {
-    await uploadFile();
-};
-  
-main();
+
 
 let browser;
 
-const request = require('request-promise')
+const request = require('request-promise');
+const { title } = require('process');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 
-async function grabConvertMp4(m3u8Url, res) {
-
-    await converter.setInputFile(m3u8Url)
-    await converter.setOutputFile("output.mp4")
-    await converter.start();
-
-    await res.download("output.mp4", async (err) => {
-        // await fs.unlinkSync("output.mp4")
-    })
-
+async function downloadM3(m3u8Url, res, req) {
+    const title = req.query.url.split("imdb=")[1]
     //res as in express.js response
+    await converter.setInputFile(m3u8Url)
+    await converter.setOutputFile(`${title}.mp4`)
+    console.log("Converting!")
 
-    // const response = await request(m3u8Url)
-    // await fs.writeFileSync("./output.m3u8", response)
-    // await res.download("./output.m3u8", async (err) => {
-    //     await fs.unlinkSync('./output.m3u8')
-    // })
+    converter.start()
+        .then(() => {
+            
+            if (uploadFile(title) == true) {
+                console.log("redirecting")
+                res.redirect(`${CDN_LINK}/${title}.mp4`)
+            } else res.redirect(`${CDN_LINK}/${title}.mp4`);
+        })
+        .catch((err) => {
+            console.log(err)
+            res.send("an error ocurred attempting to download this stream.")
+        })
+
+
+    
 
 }
 
@@ -94,70 +91,102 @@ async function grabConvertMp4(m3u8Url, res) {
     console.log("Firefox browser launched!")
 })()
 
-app.get('/download', (req, res) => {
-    (() => {
-        try {
-            this.browser.newPage()   
-                .then(async (page) => {
-                    console.log("new page created")
-                    let downloaded = false
-                    // page.on('response', response => {
-                    //     if ((response.url().startsWith("https://tmstr.vidsrc.stream/stream/")) && (downloaded == false)) {
-                    //         downloaded = true;
-                    //         page.close()
-                    //         console.log("url found in res")
+app.get('/download', async (req, res) => {
+    const {url, season, episode} = req.query;
+    if (!(url)) return res.send("no url present");
+    const title_id = url.split("imdb=")[1]
+    const urlToContent = `${CDN_LINK}/${title_id}.mp4`
+    request(urlToContent)
+        .then(() => {
+            console.log("found! redirecting")
+            return res.redirect(urlToContent)
+        })
+        .catch((err) => {
+            (() => {
+                console.log("not found in cdn. downloading...")
+                try {
+                    this.browser.newPage()   
+                        .then(async (page) => {
+                            console.log("new page created")
+                            let downloaded = false
+                            page.on('request', request => {
+                                if ((request.url().startsWith("https://tmstr2.vidsrc.stream/stream/")) && (downloaded == false)) {
+                                    downloaded = true;
+                                    page.close()
+                                    console.log(request.url())
+                                    console.log("url found in req")
+                                    return downloadM3(request.url(), res, req)
+                                    // return res.send(request.url())
+                                }
+                            });
+                            page.on('request', request => {
+                                if ((request.url().startsWith("https://tmstr.vidsrc.stream/stream/")) && (downloaded == false)) {
+                                    downloaded = true;
+                                    page.close()
+                                    console.log("url found in req")
+                                    return downloadM3(request.url(), res, req)
+                                    // return res.send(request.url())
+                                }
+                            });
+                            page.on('response', response => {
+                                if ((response.url().startsWith("https://tmstr.vidsrc.stream/stream/")) && (downloaded == false)) {
+                                    downloaded = true;
+                                    page.close()
+                                    console.log("url found in req")
+                                    return downloadM3(response.url(), res, req)
+                                    // return res.send(request.url())
+                                }
+                            });
+                            page.on('response', response => {
+                                if ((response.url().startsWith("https://tmstr2.vidsrc.stream/stream/")) && (downloaded == false)) {
+                                    downloaded = true;
+                                    page.close()
+                                    console.log("url found in req")
+                                    return downloadM3(response.url(), res, req)
+                                    // return res.send(request.url())
+                                }
+                            });
 
-                    //         return downloadM3(response.url(), res)
-                    //         // return res.send(response.url())
-                    //     }
-                    // })
-                    page.on('request', request => {
-                        if ((request.url().startsWith("https://tmstr2.vidsrc.stream/stream/")) && (downloaded == false)) {
-                            downloaded = true;
-                            page.close()
-                            console.log("url found in req")
-                            return grabConvertMp4(request.url(), res)
-                            // return res.send(request.url())
-                        }
-                    });
-                    page.on('request', request => {
-                        if ((request.url().startsWith("https://tmstr.vidsrc.stream/stream/")) && (downloaded == false)) {
-                            downloaded = true;
-                            page.close()
-                            console.log("url found in req")
-                            return grabConvertMp4(request.url(), res)
-                            // return res.send(request.url())
-                        }
-                    });
-                    const {url, season, episode} = req.query;
-                    request(url, {resolveWithFullResponse: true})
-                        .then((body) => {
-                            if (body.statusCode != 200) return res.send("Invalid movie! doesnt exist in database")
+                            request(url, {resolveWithFullResponse: true})
+                                .then((body) => {
+                                    if (body.statusCode != 200) return res.send("Invalid movie! doesnt exist in database")
+                                })
+                                .catch((err) => {
+                                    return res.send("Invalid movie! doesnt exist in database")
+                                })
+                        
+                            let full_url;
+                            if (url.split("/movie?").length == 1) full_url = `${url}&season=${season}&episode=${episode}`
+                            else full_url = url;                    
+                            page.goto(full_url)
+                                .then(() => {
+                                    page.locator("#player_iframe").click({ force: true });
+                                    page.locator("#player_iframe").click({ force: true });
+                                    page.locator("#player_iframe").click({ force: true });
+        
+                                    console.log("clicked")
+                                })
+                                .catch(err => {
+                                    res.send("error occured. please stay on this page while the file downloads.")
+                                })
                         })
-                        .catch((err) => {
-                            return res.send("Invalid movie! doesnt exist in database")
-                        })
-                
-                    let full_url;
-                    if (url.split("/movie?").length == 1) full_url = `${url}&season=${season}&episode=${episode}`
-                    else full_url = url;                    
-                    page.goto(full_url)
-                        .then(() => {
-                            page.locator("#player_iframe").click({ force: true });
-                            console.log("clicked")
-                        })
-                        .catch(err => {
-                            res.send("error occured. please stay on this page while the file downloads.")
-                        })
-                })
-            }
-            
-                
-            catch (err) {
-                console.log(err)
-                res.send("error occured. please stay on this page while the file downloads.")
-            }
-    })()
+                    }
+                    
+                        
+                    catch (err) {
+                        console.log(err)
+                        res.send("error occured. please stay on this page while the file downloads.")
+                    }
+            })()
+        })
+    // if 
+    // const cdn_status_code = ().statusCode
+    // console.log(cdn_status_code)
+    // if (cdn_status_code != 404) {
+    //     console.log("title found in cdn, redirecting")
+    //     return res.redirect(urlToContent)
+    // };
+    
 })
 
 app.listen(port, () => {
